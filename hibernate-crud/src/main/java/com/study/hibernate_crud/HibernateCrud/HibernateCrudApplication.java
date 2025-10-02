a5415f5db272696d0622b9679c0c3ca97f507bc0
susha <susha@PSS>
2025-10-02 21:03:33 +0530

    Basic project to start Hibernate crud

---
+++ hibernate-crud/src/main/java/com/study/hibernate_crud/HibernateCrud/HibernateCrudApplication.java
@@ -0,0 +1,13 @@
+package com.study.hibernate_crud.HibernateCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateCrudApplication.class, args);
+	}
+
+}
